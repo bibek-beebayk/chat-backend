8270191ce4c37214854c10addce773c703c85540
beebayk <beebayk0001@gmail.com>
2026-01-27 15:47:58 +0000

    Update prod for emails

--- chat_project/settings/prod.py
+++ chat_project/settings/prod.py
@@ -113,11 +113,19 @@ sentry_sdk.init(
     environment="chat",
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.zoho.com.au' 
-EMAIL_PORT = 587 
-EMAIL_USE_TLS = True 
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'support@hi-rollin.online'
-EMAIL_HOST_PASSWORD = "mf8D5fVaZiCT"
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.zoho.com.au' 
+# EMAIL_PORT = 587 
+# EMAIL_USE_TLS = True 
+# EMAIL_USE_SSL = False
+# EMAIL_HOST_USER = 'support@hi-rollin.online'
+# EMAIL_HOST_PASSWORD = "mf8D5fVaZiCT"
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+# ZeptoMail Configuration
+EMAIL_BACKEND = 'zepto_mail.backend.EmailBackend'
+ZEPTOMAIL_API_KEY = "GkDdjPjeqQBKwVzCoY6xNdRTbZQ0tJPrF8jgMsoC80Av791ds0wefcFBkXUuzDYa7XPDAlfLdL938DPc57WCcSwpI3qvcUTuOpwzGB+edd0FvHvXUPiy9P9gXkbKmvGpNw9m6h8x9i9g4A=="
+ZEPTO_ORG_MAIL_TOKEN = ZEPTOMAIL_API_KEY
+ZEPTOMAIL_FROM = "noreply@hrlzone.com"
+DEFAULT_FROM_EMAIL = ZEPTOMAIL_FROM
+SERVER_EMAIL = ZEPTOMAIL_FROM
